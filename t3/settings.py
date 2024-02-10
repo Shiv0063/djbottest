3b0ee30da2487ca73b1b6c98197917c91afaed54
Vishal Barot <97306828+Shiv0063@users.noreply.github.com>
2024-02-10 14:25:20 +0530

    Update

--- t3/settings.py
+++ t3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h*h_b5(tq%5!0e@l91ee3^vlo0h0lbr6ht2eaxmbv$^@0*14cc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
